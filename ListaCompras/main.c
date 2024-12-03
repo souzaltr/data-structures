0a8a5bbb0e9c6f7fa37f90c1ac3995dbeb01026e
Luiz Souza <luizsouzaa080@gmail.com>
2024-12-03 20:59:55 -0300

    Adicionando os primeiros exercícios da disciplina

---
+++ ListaCompras/main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+typedef struct lista {
+    char produto [20];
+    int quantidade;
+    struct lista* proximo;
+} ListaCompras;
+
+ListaCompras* inserirProdutos (ListaCompras* destino){
+
+    getchar();
+
+    ListaCompras* novo= (ListaCompras*) malloc(sizeof(ListaCompras));
+
+    printf("Qual produto você deseja adicionar à lista de compras? ");
+    gets(novo->produto);
+    printf("\nQual a quantidade? ");
+    scanf("%d",&novo->quantidade);
+
+    novo->proximo=destino;
+
+    return novo;
+}
+
+void imprimirLista (ListaCompras* lista){
+    ListaCompras* auxiliar;
+
+    if (lista==NULL){
+        printf("A lista de compras está vazia!");
+        return;
+    }
+
+    printf("\t\t\t\t--- Lista de Compras ---\n\n");
+
+    for(auxiliar=lista;auxiliar!=NULL;auxiliar=auxiliar->proximo){
+        printf("Produto: %s\n",auxiliar->produto);
+        printf("Produto: %d\n",auxiliar->quantidade);
+    }
+}
+
+int main () {
+
+    setlocale(LC_ALL,"Portuguese");
+
+    int escolha;
+    ListaCompras *lista1=NULL;
+
+    do {
+        system("clear"); //linux
+        //system("cls") windows
+
+        printf("\t\t\t\t--- Lista de Compras ---\n\n");
+        printf("Escolha uma opção:\n");
+        printf("1. Adicionar produto\n");
+        printf("2. Imprimir a lista\n");
+        printf("3. Sair\n");
+        printf("Digite a opção: ");
+        scanf("%d", &escolha);
+
+        switch (escolha) {
+            case 1:
+                lista1 = inserirProdutos(lista1);
+                break;
+            case 2:
+                imprimirLista(lista1);
+                break;
+            case 3:
+                printf("\nSaindo...\n");
+                break;
+            default:
+                printf("\nOpção inválida! Tente novamente.\n");
+                exit(0);
+                break;
+        }
+
+    } while (escolha != 3);
+
+    return 0;
+}
---
+++ ListaTelefonica/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+typedef struct lista{
+    char nome[50];
+    char email[50];
+    int telefone;
+    struct lista *proximo;
+} listaTelefonica;
+
+listaTelefonica* inicializar();
+listaTelefonica* inserir(listaTelefonica*);
+void imprimir(listaTelefonica*);
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+
+    listaTelefonica *lista1;
+    lista1= inicializar();
+    lista1= inserir(lista1);
+    imprimir(lista1);
+
+    return 0;
+}
+
+listaTelefonica* inicializar(){
+    return NULL;
+}
+
+listaTelefonica* inserir(listaTelefonica* destino){
+    listaTelefonica* novo= (listaTelefonica*) malloc(sizeof(listaTelefonica));
+
+    printf("\nDigite o nome a ser inserido:");
+    gets(novo->nome);
+    printf("\nDigite o email a ser inserido:");
+    scanf("%s",&novo->email);
+    fflush(stdin);
+    printf("\nDigite o telefone a ser inserido:");
+    scanf("%d",&novo->telefone);
+    fflush(stdin);
+
+    novo->proximo=destino;
+
+    return novo;
+}
+
+void imprimir(listaTelefonica* lista){
+    listaTelefonica* aux;
+
+    for(aux=lista; aux!=NULL; aux=aux->proximo){
+        printf("\nNome: %s", aux->nome);
+        printf("\nE-mail: %s", aux->email);
+        printf("\nTelefone: %d", aux->telefone);
+    }
+}
